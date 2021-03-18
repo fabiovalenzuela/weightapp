8a3f1565cf04c65942869b736473a36d2c20269b
mohfezrahman@gmail.com <Afrin0902_>
2021-03-17 21:14:38 -0400

    changed login sequencing

--- App.js
+++ App.js
@@ -8,8 +8,8 @@ export default function App() {
         const [user, setUser] = useState('');
         const [email, setEmail] = useState('');
         const [password, setPassword] = useState('');
-        const [emailError, setEmailError] = useState('Some Email Error');
-        const [passwordError, setPasswordError] = useState('Some Password Error');
+        const [emailError, setEmailError] = useState();
+        const [passwordError, setPasswordError] = useState();
         const [hasAccount, setHasAccount] = useState(true);
 
         const clearInputs = () => {
@@ -97,7 +97,7 @@ export default function App() {
             </>
         ):(
             <>
-                <Login 
+                <Login
                 email = {email}
                 setEmail = {setEmail}
                 password = {password}
