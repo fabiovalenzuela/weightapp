975d2de53c797d325f79c7a78aba3b991a2f7634
fabiovalenzuela <fvalenzuela1@ggc.edu>
2021-03-27 12:35:30 -0400

    removes import from homescreen, allows code to function as is

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,7 +1,6 @@
 import React  from "react";
 import {Text, View} from "react-native";
-import barcodeScanner from "./barcodeScanner";
-import(barcodeScanner())
+
 
 export default function HomeScreen() {
 
